addfa86cc38044f075e2d2d27022fcd973612c0c
Frank Valbuena <frankvalbuenam@gmail.com>
2020-05-12 00:57:08 -0500

    - Add SwiftPM.

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Boundaries",
+    products: [
+        .library(
+            name: "Boundaries",
+            targets: ["Boundaries"]),
+    ],
+    targets: [
+        .target(
+            name: "Boundaries",
+            dependencies: []
+        ),
+        .testTarget(
+            name: "BoundariesTests",
+            dependencies: ["Boundaries"]
+        ),
+    ]
+)
